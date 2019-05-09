c5a76c2e29fe9939d7722769634c446ea8636080
Lee, Byungman <pavane90@gmail.com>
2019-05-09 15:25:32 +0900

    ch05-13

--- app12.js
+++ app12.js
@@ -35,6 +35,52 @@ router.route('/process/product').get(function(req, res){
     }
 })
 
+router.route('/process/login').post(function(req,res){
+    console.log('process/login 라우팅 호출됨')
+    
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    console.log ('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+    
+    if (req.session.user) {
+        console.log ('이미 로그인 되어 있네요');
+        res.redirect('/public/product.html');
+    } else {
+        req.session.user = {
+            id:paramId,
+            name:'김',
+            authorized:true
+        };
+        res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
+        res.write('<h1>로그인 성공</h1>');
+        res.write('<p>Id : ' + paramId + '</p>');
+        res.write('<br><br><a href="/public/product.html">상품 페이지로 이동하기</a>');
+        res.end();
+    }
+});
+
+router.route('/process/logout').get(function(req, res){
+    console.log('/process/logout 호출')
+    
+    if(req.session.user){
+        console.log('로그아웃합니다');
+        
+        req.session.destroy(function(err){
+            if(err){
+                console.log('세션 삭제 실패' + err);
+                return;
+            }
+
+            console.log ('세션 삭제 성공');
+            res.redirect('/public/login2.html');
+        }); //세션내의 정보를 없애준다.
+    } else {
+        console.log('로그인 되어 있지 않습니다.');
+        res.redirect('/public/login2.html');
+    }
+});
+
+
 router.route('/process/setUserCookie').get(function(req, res){
     console.log('/process/setUserCookie 라우팅 함수 호출됨');
 
